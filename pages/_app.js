586d9cf09ebb0c1671601ba04d798be4abd144ad
Jonathan Shitrit <jonathan.shitrit@dowjones.com>
2021-12-28 18:05:15 -0500

    second

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import NextNprogress from "nextjs-progressbar";
+import { Router } from "next/router";
+
+// const progress = new ProgressBar({
+//   size: 4,
+//   color: "#FE595E",
+//   delay: 100,
+// });
+
+// sest;
+// Router.events.on("routeChangeStart", progress.start());
+// Router.events.on("routeChangeComplete", progress.finish());
+// Router.events.on("routeChangeError", progress.finish());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+        color="#FE595E"
+        startPosition={0.3}
+        height={3}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
